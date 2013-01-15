19522992cfb14655478c1aa5f513343ccf425ca8
chih-hsiang chang <chih-hsiangchang@chih-hsiang-changs-MacBook-Pro.local>
2013-01-15 14:12:30 -0500

    Initial Commit

---
+++ VP detection/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  VP detection
+//
+//  Created by chih-hsiang chang on 1/15/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
